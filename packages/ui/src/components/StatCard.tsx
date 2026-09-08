555e80344b15697d7c5844c09e51812119d4f562
Sophie Spang <soph8111@stud.kea.dk>
2026-09-08 08:15:33 +0200

    feat: change spacing on cards

--- packages/ui/src/components/StatCard.tsx
+++ packages/ui/src/components/StatCard.tsx
@@ -15,11 +15,11 @@ export function StatCard({
 }: StatCardProps) {
   if (variant === 'horizontal') {
     return (
-      <div className="flex gap-3 px-6">
-        <div className="w-1/3 text-6xl font-semibold text-right text-surface-content">
+      <div className="flex gap-2 px-6 items-center 2xl:px-0 justify-center">
+        <div className="text-6xl font-semibold text-right text-surface-content">
           {value}
         </div>
-        <div className="w-2/3">
+        <div className=" 2xl:max-w-3/5">
           <div className="text-surface-content">{label}</div>
           {sublabel && (
             <div className="text-surface-content-muted">{sublabel}</div>
@@ -30,7 +30,7 @@ export function StatCard({
   }
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 ">
       <div className="text-surface-content-muted">{label}</div>
       <div className="flex items-baseline gap-1">
         <span className="text-6xl font-semibold text-surface-content">
