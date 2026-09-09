817fc3899478d8c7df95f582ec2e4626805ecd57
Sophie Spang <soph8111@stud.kea.dk>
2026-09-09 10:28:31 +0200

    feat: part of the runner-app flow commit

--- packages/shared/src/sanity/queries/teamQueries.ts
+++ packages/shared/src/sanity/queries/teamQueries.ts
@@ -21,7 +21,7 @@ export const getTeamStandingsQuery = /* groq */ `
     "teams": *[_type == "team" && year == $year]{
       _id,
       teamName,
-      "runnerIds": runners[]._ref
+      "runnerIds": runners[]._rsef
     },
     "results": *[_type == "result" && team->year == $year && defined(result)]{
       "runnerId": runner._ref,
