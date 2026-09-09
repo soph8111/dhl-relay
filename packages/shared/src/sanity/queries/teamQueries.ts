d0645a8e9e0114c312ef47d49e1ba82abd9da47f
Sophie Spang <soph8111@stud.kea.dk>
2026-09-09 10:45:05 +0200

    fix: correct typo (_rsef -> _ref) in getTeamStandingsQuery breaking all team standings

--- packages/shared/src/sanity/queries/teamQueries.ts
+++ packages/shared/src/sanity/queries/teamQueries.ts
@@ -21,7 +21,7 @@ export const getTeamStandingsQuery = /* groq */ `
     "teams": *[_type == "team" && year == $year]{
       _id,
       teamName,
-      "runnerIds": runners[]._rsef
+      "runnerIds": runners[]._ref
     },
     "results": *[_type == "result" && team->year == $year && defined(result)]{
       "runnerId": runner._ref,
