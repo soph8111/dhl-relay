8b7f824f9230a2c3964b523b7ef2c720e9bb40fe
Sophie Spang <soph8111@stud.kea.dk>
2026-09-15 10:29:05 +0200

    feat: add build for render deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -176,6 +176,7 @@ setInterval(() => {
   }
 }, TIMEOUT_CHECK_INTERVAL_MS);
 
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
