9db762ce23ad9933bd9329a23451c08e0bd19503
Sophie Spang <soph8111@stud.kea.dk>
2026-09-15 12:20:30 +0200

    fix: adding ngrok to allowed host for realtime testning

--- runner-app/vite.config.ts
+++ runner-app/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: { port: 5174 },
+  server: { port: 5174, host: true, allowedHosts: ['.ngrok-free.dev'] },
   resolve: {
     tsconfigPaths: true,
   },
