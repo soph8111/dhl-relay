613c78b529fef27ab8d46a6de355c329686446b6
Sophie Spang <soph8111@stud.kea.dk>
2026-09-15 10:12:47 +0200

    fix: bug with .env file

--- runner-app/src/vite-env.d.ts
+++ runner-app/src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_SANITY_PROJECT_ID: string;
   readonly VITE_SANITY_DATASET: string;
   readonly VITE_CARTO_API_KEY: string;
+  readonly VITE_SERVER_URL: string;
 }
 
 interface ImportMeta {
